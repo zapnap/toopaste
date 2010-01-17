e7ca8706d59c900392c7aded9c443ce8da7ab145
David Everitt <deveritt@innotts.co.uk>
2010-01-17 10:59:38 +0000

    Add updated ruby file, views and new README.

--- toopaste.rb
+++ toopaste.rb
@@ -1,3 +1,5 @@
+#!/usr/local/bin/ruby -rubygems
+
 require 'sinatra'
 require 'dm-core'
 require 'dm-validations'
@@ -9,22 +11,25 @@ DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/toopaste.sqlite3")
 class Snippet
   include DataMapper::Resource
 
-  property :id,         Integer, :serial => true    # primary serial key
-  property :body,       Text,    :nullable => false # cannot be null
+  property :id,         Serial # primary serial key
+  property :title,      String, :required => true, :length => 32
+  property :body,       Text,   :required => true
   property :created_at, DateTime
   property :updated_at, DateTime
-  
-  #validates_present :body
-  #validates_length :body, :minimum => 1
-  
+
+  # validates_present :body
+  # validates_length :body, :minimum => 1
+
   Syntaxi.line_number_method = 'floating'
   Syntaxi.wrap_at_column = 80
   #Syntaxi.wrap_enabled = false
-  
+
   def formatted_body
     replacer = Time.now.strftime('[code-%d]')
-    html = Syntaxi.new("[code lang='ruby']#{self.body.gsub('[/code]', replacer)}[/code]").process
-    "<div class=\"syntax syntax_ruby\">#{html.gsub(replacer, '[/code]')}</div>"
+    html = Syntaxi.new("[code lang='ruby']#{self.body.gsub('[/code]',
+    replacer)}[/code]").process
+    "<div class=\"syntax syntax_ruby\">#{html.gsub(replacer, 
+    '[/code]')}</div>"
   end
 end
 
@@ -38,7 +43,8 @@ end
 
 # create
 post '/' do
-  @snippet = Snippet.new(:body => params[:snippet_body])
+  @snippet = Snippet.new(:title => params[:snippet_title],
+                         :body  => params[:snippet_body])
   if @snippet.save
     redirect "/#{@snippet.id}"
   else
